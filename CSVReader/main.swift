a243c0d1a2929aae414736dc08b23635ce4f18fd
Peter Entwistle <petertheanimator@gmail.com>
2015-03-06 22:20:59 +0000

    Initial Commit

---
+++ CSVReader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CSVReader
+//
+//  Created by Peter Entwistle on 06/03/2015.
+//  Copyright (c) 2015 Peter Entwistle. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
