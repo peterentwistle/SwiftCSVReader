f121876db696db7e323b3ebff0e96a8cbc388adb
Peter Entwistle <petertheanimator@gmail.com>
2015-08-07 19:23:26 +0100

    Fixed module name issue

--- CSVReader/CSVReader.h
+++ CSVReader/CSVReader.h
@@ -1,12 +1,15 @@
 //
-//  Source.h
-//  Source
+//  CSVReader.h
+//  CSVReader
 //
-//  Created by Peter Entwistle on 06/08/2015.
+//  Created by Peter Entwistle on 07/08/2015.
 //  Copyright © 2015 Peter Entwistle. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
+//! Project version number for CSVReader.
 FOUNDATION_EXPORT double CSVReaderVersionNumber;
+
+//! Project version string for CSVReader.
 FOUNDATION_EXPORT const unsigned char CSVReaderVersionString[];
